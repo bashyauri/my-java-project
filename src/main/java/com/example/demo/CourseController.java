9f04fcc093a85a3a4643f028f586222342a92898
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-14 10:58:34 +0100

    added more courses

--- src/main/java/com/example/demo/CourseController.java
+++ src/main/java/com/example/demo/CourseController.java
@@ -12,7 +12,9 @@ public class CourseController {
     public List<Course> retrieveAllCourses() {
         return Arrays.asList(
             new Course(1,"Learn AWS","in28Minutes"),
-            new Course(2,"Learn DevOps","in28Minutes")
+            new Course(2,"Learn DevOps","in28Minutes"),
+                new Course(3, "Learn Microsoft Azure", "in28Minutes"),
+                new Course(4,"Learn Microsoft Azure","in28Minutes")
         );
     }
     
